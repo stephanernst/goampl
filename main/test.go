629988a19a14bf210c105fb238467526cfebb2dd
stephanernst <stephan_ernst@live.com>
2014-10-24 02:21:44 -0500

    Added more print tests

--- main/test.go
+++ main/test.go
@@ -5,7 +5,7 @@ import "fmt"
 
 func main() {
 
-	model:= goampl.AMPL_init("ch3.nl")
+	model:= goampl.AMPL_init("dietd.nl")
 	fmt.Println("Problem name:")
 	fmt.Println(model.Name)
 	fmt.Println("\nNumber of variables:")
@@ -38,6 +38,39 @@ func main() {
 	for j:=0; j<model.Nvar; j++ {
 		fmt.Println(model.Var_name[j])
 	}
+
+	fmt.Println("\nVariables in Objectives")
+	for i:=0; i < model.Nobj; i++ {
+		fmt.Printf("\nObjective %v: ", i)
+		for j:=0; j < len(model.Obj[i]); j++ {
+			fmt.Printf("%v, ", model.Obj[i][j])		
+		}	
+	}
+
+	fmt.Println("\nVariables in Constraints")
+	for i:=0; i < model.Ncon; i++ {
+		fmt.Printf("\nConstraint %v: ", i)
+		for j:=0; j < len(model.Cons[i]); j++ {
+			fmt.Printf("%v, ", model.Cons[i][j])		
+		}	
+	}
+
+	fmt.Println("\n\nObjectives that use the Variable")
+	for i:=0; i < model.Nvar; i++ {
+		fmt.Printf("\nVariable %v: ", i)
+		for j:=0; j < len(model.Varobj[i]); j++ {
+			fmt.Printf("%v, ", model.Varobj[i][j])		
+		}	
+	}
+
+	fmt.Println("\n\nConstraints that use the Variable")
+	for i:=0; i < model.Nvar; i++ {
+		fmt.Printf("\nVariable %v: ", i)
+		for j:=0; j < len(model.Varcons[i]); j++ {
+			fmt.Printf("%v, ", model.Varcons[i][j])		
+		}	
+	}
+
 	point := make([]float64, model.Nvar)
 	fmt.Println("\nTest Functions:")
 	for i:=0; i<model.Nvar; i++ {
